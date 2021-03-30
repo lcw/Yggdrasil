2bb9da2c76a396edb3ba11248cef2e8b2c3eb8fb
Lucas C Wilcox <lucas@swirlee.com>
2021-03-30 15:27:28 -0700

    Enable Fortran in OpenMPI
    The build is currently complaining so we have added some debug
    output to the `configure` script via a patch which inserts `set -x` and
    `set +x`.

--- O/OpenMPI/build_tarballs.jl
+++ O/OpenMPI/build_tarballs.jl
@@ -5,13 +5,15 @@ version = v"4.0.2"
 sources = [
     ArchiveSource("https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-$(version).tar.gz",
                   "662805870e86a1471e59739b0c34c6f9004e0c7a22db068562d5388ec4421904"),
+     DirectorySource("./bundled"),
 ]
 
 script = raw"""
 # Enter the funzone
 cd ${WORKSPACE}/srcdir/openmpi-*
 
-./configure --prefix=$prefix --host=$target --enable-shared=yes --enable-static=no --disable-mpi-fortran --without-cs-fs
+atomic_patch -u -p1 ${WORKSPACE}/srcdir/patches/debug_configure.patch
+./configure ompi_cv_fortran_sizeof_CHARACTER=1 --prefix=$prefix --host=$target --enable-shared=yes --enable-static=no --without-cs-fs
 
 # Build the library
 make "${flags[@]}" -j${nproc}
@@ -27,6 +29,7 @@ platforms = filter(!Sys.iswindows, supported_platforms())
 
 products = [
     LibraryProduct("libmpi", :libmpi)
+    LibraryProduct("libmpi_mpifh", :libmpi_mpifh)
     ExecutableProduct("mpiexec", :mpiexec)
 ]
 
